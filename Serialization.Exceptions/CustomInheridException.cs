f0f9ac17c28224c42ecd6fe12bf2d5f6f9ab290e
m4tchl0ck <m4tchl0ck@outlook.com>
2024-12-09 13:06:15 +0000

    Use surrogateType for custom inherit exception serialization

--- Serialization.Exceptions/CustomInheridException.cs
+++ Serialization.Exceptions/CustomInheridException.cs
@@ -5,3 +5,28 @@ public class CustomInheridException : AbstractException
     {
     }
 }
+
+
+[GenerateSerializer]
+public struct CustomInheridExceptionTypeSurrogate
+{
+    [Id(0)]
+    public string Message;
+}
+
+// This is a converter that converts between the surrogate and the foreign type.
+[RegisterConverter]
+public sealed class CustomInheridExceptionTypeSurrogateConverter :
+    IConverter<CustomInheridException, CustomInheridExceptionTypeSurrogate>
+{
+    public CustomInheridException ConvertFromSurrogate(
+        in CustomInheridExceptionTypeSurrogate surrogate) =>
+        new(surrogate.Message);
+
+    public CustomInheridExceptionTypeSurrogate ConvertToSurrogate(
+        in CustomInheridException value) =>
+        new()
+        {
+            Message = value.Message
+        };
+}
