2ecea4c95c1a9e40a477efa3351fe6e8c6e2cf73
m4tchl0ck <m4tchl0ck@outlook.com>
2024-12-09 12:58:26 +0000

    Use surrogateType for custom exception serialization

--- Serialization.Exceptions/CustomException.cs
+++ Serialization.Exceptions/CustomException.cs
@@ -4,3 +4,27 @@ public class CustomException : Exception
     {
     }
 }
+
+[GenerateSerializer]
+public struct CustomExceptionTypeSurrogate
+{
+    [Id(0)]
+    public string Message;
+}
+
+// This is a converter that converts between the surrogate and the foreign type.
+[RegisterConverter]
+public sealed class CustomExceptionTypeSurrogateConverter :
+    IConverter<CustomException, CustomExceptionTypeSurrogate>
+{
+    public CustomException ConvertFromSurrogate(
+        in CustomExceptionTypeSurrogate surrogate) =>
+        new(surrogate.Message);
+
+    public CustomExceptionTypeSurrogate ConvertToSurrogate(
+        in CustomException value) =>
+        new()
+        {
+            Message = value.Message
+        };
+}
