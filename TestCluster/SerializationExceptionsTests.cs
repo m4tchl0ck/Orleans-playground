4d3f0a824dae567b2a37dd9d7f4c01fedbea704a
m4tchl0ck <m4tchl0ck@outlook.com>
2025-01-03 21:38:22 +0000

    Add tests for Serialization.Exception

---
+++ TestCluster/SerializationExceptionsTests.cs
@@ -0,0 +1,37 @@
+using FluentAssertions;
+using Orleans.TestingHost;
+
+public class SerializationExceptionsTests : IAsyncLifetime
+{
+    TestCluster TestCluster { get; }
+
+    public SerializationExceptionsTests()
+    {
+        var builder = new TestClusterBuilder(1);
+        TestCluster = builder.Build();
+    }
+
+    [Fact]
+    public async Task Test1()
+    {
+        // Arrange
+        var grain = TestCluster.GrainFactory.GetGrain<IThrowingGrain>(Guid.NewGuid().ToString());
+
+        // Act
+        var act = async () => await grain.ThrowCustomException();
+
+        // Assert
+        await act.Should().ThrowAsync<CustomException>();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await TestCluster.DeployAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await TestCluster.StopAllSilosAsync();
+        await TestCluster.DisposeAsync();
+    }
+}
