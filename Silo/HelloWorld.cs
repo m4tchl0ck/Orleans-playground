361e390918dc243839c5e7d662e6b4b135186fdc
m4tchl0ck <m4tchl0ck@outlook.com>
2025-01-30 21:47:07 +0000

    Add HelloWorld grain

---
+++ Silo/HelloWorld.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.Logging;
+
+public interface IHelloWorld : IGrainWithStringKey
+{
+    Task<string> SayHello(string greeting);
+}
+
+public class HelloWorld(ILogger<HelloWorld> logger) : Grain, IHelloWorld
+{
+    public Task<string> SayHello(string greeting)
+    {
+        logger.LogInformation("[[{grainId}] SayHello message received: greeting = '{greeting}'", this.GetPrimaryKeyString(), greeting);
+        return Task.FromResult($"You said: '{greeting}', I say: Hello!");
+    }
+}
